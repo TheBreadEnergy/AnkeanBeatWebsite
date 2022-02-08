72436320f210c13f2b0e12602013386e373554c7
Amovet <57827473+Amovet@users.noreply.github.com>
2022-02-08 13:55:51 +0500

    Add first version website

---
+++ JS/js.js
@@ -0,0 +1,37 @@
+const button_play = document.getElementById('Button_play')
+const canvas = document.getElementById('canvas1')
+const ctx = canvas.getContext('2d')
+let audioSource
+let analyser
+
+button_play.addEventListener('click', function () {
+	let audio1 = new Audio('/Audio/button-15.wav')
+	const audioContext = new AudioContext()
+	audio1.play()
+	audioSource = audioContext.createMediaElementSource(audio1)
+	analyser = audioContext.createAnalyser()
+	audioSource.connect(analyser)
+	analyser.connect(audioContext.destination)
+	analyser.fftSize = 128
+	const bufferLength = analyser.frequencyBinCount
+	const dataArray = new Uint8Array(bufferLength)
+
+	const barWidth = canvas.width / bufferLength
+	let barHeight
+	let x
+
+	function animate() {
+		x = 1
+		ctx.clearRect(0, 0, canvas.width, canvas.height)
+		analyser.getByteFrequencyData(dataArray)
+		console.log(dataArray)
+		for (let i = 0; i < bufferLength; i++) {
+			barHeight = dataArray[i] / 4
+			ctx.fillStyle = 'white'
+			ctx.fillRect(x, canvas.height - barHeight, barWidth, barHeight)
+			x += barWidth
+		}
+		requestAnimationFrame(animate)
+	}
+	animate()
+})
